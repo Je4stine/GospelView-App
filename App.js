438e34047899c383bc69bce288d40ca715314521
JEFF <je4stine@gmail.com>
2021-06-06 11:15:25 +0300

    Changes

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import MainNavigation from './navigation';
 import { Provider } from 'react-redux';
 import store from './store';
+import { StatusBar } from 'react-native';
 
 
 const App = () => {
   return (
     <Provider store={store}>
+      <StatusBar backgroundColor="#1A1A1A" barStyle="light-content" />
       <MainNavigation />
     </Provider>
   ); 
